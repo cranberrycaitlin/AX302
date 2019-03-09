713ee68bc512fe6df8f658251f69e7272a9e2e5c
cranberrycaitlin <peppermintcranberryyalayada@gmail.com>
2019-03-09 12:08:21 +0800

    lesson8

--- lesson8/app.js
+++ lesson8/app.js
@@ -66,7 +66,7 @@ function create(){
 	stars = game.add.physicsGroup();
 	stars.enableBody = true;
 	// we will create 12 stars evenly spaced
-	for (var i = 0; i < 50; i++){
+	for (var i = 0; i < 12; i++){
 		var star = stars.create(i * 70, 0, 'star');
 		star.body.gravity.y = 200;
 		star.body.bounce.y = 0.7 + Math.random() * 0.2;
